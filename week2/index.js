28c8ef08cf81e900e0699ff59d854e6a858dc260
Christian Van Loon <66512995+WarMachine-282@users.noreply.github.com>
2020-07-30 21:28:45 -0700

    first commit

---
+++ week2/index.js
@@ -0,0 +1,18 @@
+// console.log(typeof 42);
+
+// console.log(typeof 'blubber');
+
+// console.log(typeof true);
+
+// console.log(typeof undeclaredVariable);
+
+// console.log(typeof "42" === "number" ? "green is true": "red is false")
+
+// console.log(42 === "42"? "if they are the same": "if they are false")
+var condition = 42 === "42"
+console.log(condition)
+// if (condition){
+    console.log(2-"2")
+// } else{
+    console.log(2+2)
+// }
