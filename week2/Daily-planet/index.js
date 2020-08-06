86495fbe2c592a1c62fe503953eab77c800e0d74
Christian Van Loon <66512995+WarMachine-282@users.noreply.github.com>
2020-08-06 11:54:14 -0700

    first commit

---
+++ week2/Daily-planet/index.js
@@ -0,0 +1,40 @@
+/*
+This code contains only syntax and code style problems. The logic of the code works,
+so DO NOT change the functionality of anything in here.
+In short, you shouldn't need to add your own statements anywhere,
+just fix the existing ones.
+*/
+
+
+// Written by Kent, Clark
+
+var enemies= ["Lex","Batman","Darkseid","Brainiac","General Zod","Doomsday"];
+
+function whoWins(isThereKryptonite, enemyName) { 
+    if (!isThereKryptonite) { 
+    return "Superman beats " + enemyName + ", of course";
+    } else {
+    return "Depends on how quick Superman can get rid of the Kryptonite. " + enemyName + " could possibly win this one.";
+}
+    }
+
+function howAttractedIsLoisLaneToMe ()
+{
+    // 1 is not at all attracted, 10 is "super" attracted...
+    return Math.floor((Math.random()*10)+1);
+}
+
+consol.log(howAttractedIsLoisLaneToMe ()) ;
+
+var clarkKent = true;
+var superman = false;
+
+while (clarkKent){
+    console.log("I'm just a nerdy columnist");
+    var phoneBoothQuickChange = Math.random();
+    if (phoneBoothQuickChange >= 0.5) {
+        clarkKent=false;
+        superman=true;
+        console.log("Now I'm Superman!");
+    }
+}
